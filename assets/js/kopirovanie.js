16a42ad465ef89788eb5c6565aeb08754fc3ec4a
GreenyBeany <matusondrejka3@gmail.com>
2024-02-18 15:25:48 +0100

    Update

---
+++ assets/js/kopirovanie.js
@@ -0,0 +1,12 @@
+function copy(copyId){
+    let inputElement = document.createElement("input");
+    inputElement.type = "text";
+    let copyText = document.getElementById(copyId).innerHTML;
+    inputElement.value = copyText;
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputElement);
+    
+    alert("Akademický mail bol skopírovaný!");
+}
