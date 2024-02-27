04d52be31ca4a17a029b2286b1beec96fd76fbd8
GreenyBeany <matusondrejka3@gmail.com>
2024-02-27 16:12:14 +0100

    Update

--- assets/bootstrap/js/function.js
+++ assets/bootstrap/js/function.js
@@ -1,13 +1,24 @@
 window.onload = function() {
     const loader = document.getElementById("loader");
-    loader.parentNode.removeChild(loader);
+  
+    // Fade out the loader
+    let opacity = 1;
+    const fadeInterval = setInterval(() => {
+      opacity -= 0.1; // Adjust fade speed as needed
+      loader.style.opacity = opacity;
+      if (opacity <= 0) {
+        clearInterval(fadeInterval);
+        loader.parentNode.removeChild(loader);
+      }
+    }, 50); // Adjust fade duration as needed
+  
     const navbar = document.getElementsByClassName("navbar");
-    
+  
     // Check if at least one navbar element is found
     if (navbar.length > 0) {
-        // Add the "fixed-top" class to the first navbar element
-        navbar[0].classList.add("fixed-top");
+      // Add the "fixed-top" class to the first navbar element
+      navbar[0].classList.add("fixed-top");
     } else {
-        console.error("No navbar element found with class name 'navbar'");
-  }
-};
+      console.error("No navbar element found with class name 'navbar'");
+    }
+  };
