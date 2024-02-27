679f85bfce9f601ac344a66d56d889f41a21e2d6
GreenyBeany <matusondrejka3@gmail.com>
2024-02-27 15:57:52 +0100

    update

--- assets/bootstrap/js/function.js
+++ assets/bootstrap/js/function.js
@@ -1,4 +1,13 @@
 window.onload = function() {
     const loader = document.getElementById("loader");
     loader.parentNode.removeChild(loader);
-  };
+    const navbar = document.getElementsByClassName("navbar");
+    
+    // Check if at least one navbar element is found
+    if (navbar.length > 0) {
+        // Add the "fixed-top" class to the first navbar element
+        navbar[0].classList.add("fixed-top");
+    } else {
+        console.error("No navbar element found with class name 'navbar'");
+  }
+};
